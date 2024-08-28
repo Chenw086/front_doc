1c9695186a2d7d6e09ec23ff368f3e23996cbb9c
chenwei <viviichen@tencent.com>
2024-08-28 16:45:34 +0800

    feat(nuxt_study): 1. 初始化项目 2. 服务端与客户端

---
+++ code/nuxt_study/nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+// 就相当于 vite-config 的作用一样
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true }
+})
