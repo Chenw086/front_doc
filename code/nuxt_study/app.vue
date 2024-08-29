583ef19745369dcf9dc6a589d9e713b16b275402
chenwei <viviichen@tencent.com>
2024-08-29 15:07:22 +0800

    feat(nuxt_stydy): 在 vue 中使用 scss 定义的变量

--- code/nuxt_study/app.vue
+++ code/nuxt_study/app.vue
@@ -1,32 +1,17 @@
 <template>
-	<div>你好陈伟 {{ name }}</div>
-	<button @click="test">测试按钮</button>
-	<button @click="test1">弹窗测试</button>
+	<h1>你好陈伟</h1>
+  <p>test</p>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-const name = ref('aaa')
-
-console.log(1111)
-// 在浏览器控制台打印一次，在终端打印一次
-// 因为用户请求的时候，会在服务端先渲染一次，此时会在终端打印，然后给前端， 前端渲染的时候再打印一次
-
-const test = () => {
-	console.log('打印测试')
-}
-// 这个打印是正常的， 请求后点击按钮是在客户端渲染
-
-// for (let i = 0; i < 2; i++) {
-// 	alert('弹窗', i)
-// }
-// ctrl + s 会正常渲染，因为 hmr 只体现在客户端
-// 但是此时重新请求页面，就会报错，因为此时会重新渲染，读 alter 而在服务端是没有 alter 的，就会报错
-
-// 但是上面内容包含在按钮函数里面就没事，此时服务端渲染不会运行函数里面的内容
-const test1 = () => {
-  for (let i = 0; i < 2; i++) {
-	alert('弹窗', i)
-}
-}
+// 这个函数拿到全局配置
+const config = useRuntimeConfig()
+console.log(config.count)  // 重新访问的时候，只能在服务端取到，客户端会打印 ...
+console.log(config.public.baseURL)  // public 里面定义的，既能在服务端也能在客户端取到
 </script>
+
+<style lang="scss">
+p{
+  color: $colorTest;
+}
+</style>
--- code/nuxt_study/nuxt.config.ts
+++ code/nuxt_study/nuxt.config.ts
@@ -1,6 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-// 就相当于 vite-config 的作用一样
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  // nuxt 中根目录使用 ~表示, 初始化样式，在 app.vue 里面无法使用里面定义的变量
+  // 想要使用就必须在 vite 里面使用
+  // css: ['~/assets/css/base.scss'],
+
+  // 以下这样定义以后就能在 app.vue 里面使用定义的 css 变量了
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/css/base.scss" as *;'
+        }
+      }
+    }
+  },
+
+  // 运行时的一些全局变量
+  runtimeConfig: {
+    // 只能在服务端取到
+    count: 1,
+
+    // 既能在服务端也能在客户端
+    public: {
+      baseURL: 'localhost:8080'
+    }
+  }
 })
