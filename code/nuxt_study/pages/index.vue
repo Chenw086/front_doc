3d3e9a82229618e38ce8391feebe8d46d46182c7
chenwei <viviichen@tencent.com>
2024-08-30 00:51:28 +0800

    feat(nuxt_study): 编程式路由

--- code/nuxt_study/pages/index.vue
+++ code/nuxt_study/pages/index.vue
@@ -1,8 +1,17 @@
 <template>
 	<div>user/</div>
+	<el-button @click="() => router.push('/login')"
+		>index 页面跳转</el-button
+	>
 </template>
 
 <script setup>
+const router = useRouter()
+
+// 直接在下面写也会直接在客户端执行执行，如果在下面打印，在客户端不会运行，但是会在服务端运行
+// router.push('/login')
+// console.log(123)
+
 onMounted(() => {
 	// 查询数据然后渲染页面
 	// onMounted 这个周期是已经渲染完成以后，再请求， 这里面的渲染就是客户端阶段了。
