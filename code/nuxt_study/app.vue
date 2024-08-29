152772a8fae733175d62ce74c6d6ae8b050eba62
chenwei <viviichen@tencent.com>
2024-08-29 16:01:12 +0800

    feat(nuxt_study): 简单路由

--- code/nuxt_study/app.vue
+++ code/nuxt_study/app.vue
@@ -1,31 +1,20 @@
 <template>
-	<h1>你好陈伟</h1>
-  <p>test</p>
+	<h1>路由学习</h1>
+
+  <!-- 路由的跳转是在客户端进行的 -->
+  <NuxtLink to="/login">登陆页面</NuxtLink>
+  <br>
+  <NuxtLink to="/">首页</NuxtLink>
+
+  <!-- 这个在 nuxt 里面不能正常的运行 -->
+  <!-- <router-view></router-view> -->
+   <div>
+    <nuxtPage></nuxtPage>
+   </div>
 </template>
 
 <script setup>
-// 这个函数拿到全局配置
-const config = useRuntimeConfig()
-console.log(config.count)  // 重新访问的时候，只能在服务端取到，客户端会打印 ...
-console.log(config.public.baseURL)  // public 里面定义的，既能在服务端也能在客户端取到
 
-// 这是自己配置变量的方式，不推荐
-// if(config.isServer) {
-//   console.log('当前在 服务端')
-// } else {
-//   console.log('当前在客户端')
-// }
-
-// 使用变量获取
-if(import.meta.server){
-  console.log('当前在 服务端')
-} else {
-  console.log('当前在客户端')
-}
 </script>
 
-<style lang="scss">
-p{
-  color: $colorTest;
-}
-</style>
+<style lang="scss"></style>
--- code/nuxt_study/nuxt.config.ts
+++ code/nuxt_study/nuxt.config.ts
@@ -1,36 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
+	compatibilityDate: '2024-04-03',
+	devtools: { enabled: true },
 
-  // nuxt 中根目录使用 ~表示, 初始化样式，在 app.vue 里面无法使用里面定义的变量
-  // 想要使用就必须在 vite 里面使用
-  // css: ['~/assets/css/base.scss'],
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: '@use "~/assets/css/base.scss" as *;'
+				}
+			}
+		}
+	},
 
-  // 以下这样定义以后就能在 app.vue 里面使用定义的 css 变量了
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: '@use "~/assets/css/base.scss" as *;'
-        }
-      }
-    }
-  },
+	// 引入 element-plus
+	modules: ['@element-plus/nuxt'],
 
-  // 引入 element-plus
-  modules: ['@element-plus/nuxt'],
-
-  // 运行时的一些全局变量
-  runtimeConfig: {
-    // 只能在服务端取到
-    count: 1,
-
-    isServer: true,
-
-    // 既能在服务端也能在客户端
-    public: {
-      baseURL: 'localhost:8080'
-    }
-  }
+	// 运行时的一些全局变量
+	runtimeConfig: {
+		public: {}
+	}
 })
